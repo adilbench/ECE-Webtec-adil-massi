fe2054392bf95f9d3687fe2d8ec4012359692c3d
adilbench <79632595+adilbench@users.noreply.github.com>
2023-11-06 22:05:25 +0100

    @rahem-massinissa
    update about.jsx

--- LAB4/pages/about.jsx
+++ LAB4/pages/about.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
 
-function about() {
+function App() {
   return (
-    <div>
-      <h1>about</h1>
-      {/* Contenu de la page d'accueil */}
-    </div>
+    <html lang="fr">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="acuile-1.css" />
+        <link rel="icon" type="image/png" href="voiture-de-sport.png" />
+        <title>Passionnés d'Automobiles</title>
+      </head>
+      <body>
+        <header>
+          <h1>AutoPass</h1>
+        </header>
+        <nav>
+          <ul>
+            <li><a href="acuille.html">Accueil</a></li>
+            <li><a href="Article.html">Articles</a></li>
+            <li><a href="/galerie">Galerie</a></li>
+          </ul>
+        </nav>
+        <main>
+          <h2>Bienvenue sur notre site pour les passionnés d'automobiles !</h2>
+          <p>Découvrez les dernières actualités, articles et photos de voitures.</p>
+
+          <section className="gallery">
+            <h3>Galerie de Voitures</h3>
+            <div className="image-container">
+              <img src="Ferrari-Testarossa-1.png" alt="Ferrari Testarossa" />
+              <img src="mclaren-f1.jpg" alt="mclaren f1" />
+              <img src="mercedes-slr-mclaren-stirling-moss-4.jpg" alt="mercedes-slr-mclaren-stirling-moss" />
+              <img src="first-aston-martin-db5-.jpg" alt="aston-martin-db5s" />
+            </div>
+          </section>
+        </main>
+
+        <footer>
+          © 2023 AutoPass
+        </footer>
+      </body>
+    </html>
   );
 }
 
-export default about;
+export default App;
